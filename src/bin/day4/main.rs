34f94a94663b1cdf9767e122e85cce586d1aa7b9
jack-cooper <cooper.jackwilliam@gmail.com>
2022-12-04 22:13:50 +0000

    chore(day4): Cleaned up solution for day4

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -1,6 +1,8 @@
-use std::{fs, io};
+use std::{error::Error, fs};
 
-fn main() -> io::Result<()> {
+use adventofcode_2022::CustomError;
+
+fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("src/bin/day4/input.txt")?;
 
     part1(&input);
@@ -13,19 +15,30 @@ fn part1(input: &str) {
     let num_overlapping_sections = input
         .lines()
         .map(|section_pair| section_pair.split(','))
-        .map(|section_ranges| {
-            let mut section_ranges = section_ranges.map(|section_range| {
-                let mut boundaries = section_range.split('-').flat_map(str::parse::<u16>); // Questions here
+        .flat_map(|section_ranges| {
+            let mut section_ranges = section_ranges.flat_map(|section_range| {
+                let mut boundaries = section_range.split('-').flat_map(str::parse::<u16>);
 
-                let (start, end) = (boundaries.next().unwrap(), boundaries.next().unwrap());
+                let (start, end) = (
+                    boundaries
+                        .next()
+                        .ok_or_else(|| CustomError::new("Missing a start for a range."))?,
+                    boundaries
+                        .next()
+                        .ok_or_else(|| CustomError::new("Missing an end for a range."))?,
+                );
 
-                start..=end
+                Ok::<_, CustomError>(start..=end)
             });
 
-            (
-                section_ranges.next().unwrap(),
-                section_ranges.next().unwrap(),
-            )
+            Ok::<_, CustomError>((
+                section_ranges.next().ok_or_else(|| {
+                    CustomError::new("Missing the first of a section range pair.")
+                })?,
+                section_ranges.next().ok_or_else(|| {
+                    CustomError::new("Missing the second of a section range pair.")
+                })?,
+            ))
         })
         .filter(|(first_range, second_range)| {
             if first_range.len() > second_range.len() {
@@ -45,19 +58,30 @@ fn part2(input: &str) {
     let num_overlapping_sections = input
         .lines()
         .map(|section_pair| section_pair.split(','))
-        .map(|section_ranges| {
-            let mut section_ranges = section_ranges.map(|section_range| {
-                let mut boundaries = section_range.split('-').flat_map(str::parse::<u16>); // Questions here
+        .flat_map(|section_ranges| {
+            let mut section_ranges = section_ranges.flat_map(|section_range| {
+                let mut boundaries = section_range.split('-').flat_map(str::parse::<u16>);
 
-                let (start, end) = (boundaries.next().unwrap(), boundaries.next().unwrap());
+                let (start, end) = (
+                    boundaries
+                        .next()
+                        .ok_or_else(|| CustomError::new("Missing a start for a range."))?,
+                    boundaries
+                        .next()
+                        .ok_or_else(|| CustomError::new("Missing an end for a range."))?,
+                );
 
-                start..=end
+                Ok::<_, CustomError>(start..=end)
             });
 
-            (
-                section_ranges.next().unwrap(),
-                section_ranges.next().unwrap(),
-            )
+            Ok::<_, CustomError>((
+                section_ranges.next().ok_or_else(|| {
+                    CustomError::new("Missing the first of a section range pair.")
+                })?,
+                section_ranges.next().ok_or_else(|| {
+                    CustomError::new("Missing the second of a section range pair.")
+                })?,
+            ))
         })
         .filter(|(first_range, second_range)| {
             if first_range.len() > second_range.len() {
