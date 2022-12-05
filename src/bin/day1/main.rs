dfa18d9ddab48247b7b2c1c2e08b140ca06d1f72
jack-cooper <jack.cooper@stratumfive.com>
2022-12-05 14:33:24 +0000

    chore(day1): Improved error handling and reuse of shared logic

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,47 +1,59 @@
-use std::{fs, io};
+use std::{fs, num::ParseIntError};
 
-fn main() -> io::Result<()> {
+use adventofcode_2022::AnyResult;
+
+fn main() -> AnyResult {
     let input = fs::read_to_string("src/bin/day1/input.txt")?;
 
-    part1(&input);
-    part2(&input);
+    part1(&input)?;
+    part2(&input)?;
 
     Ok(())
 }
 
-fn part1(input: &str) {
-    let elves = input.split("\n\n");
-
-    let max_calories: Option<u32> = elves
-        .map(|elf| elf.lines().flat_map(str::parse::<u32>).sum())
-        .max();
-
-    if let Some(calories) = max_calories {
-        println!("Part 1 answer = {calories}");
-    }
+fn elf_to_calories(elf: &str) -> Result<u32, ParseIntError> {
+    elf.lines().map(str::parse::<u32>).sum()
 }
 
-fn part2(input: &str) {
+fn part1(input: &str) -> AnyResult {
+    let elves = input.split("\n\n");
+
+    let max_calories = elves
+        .map(elf_to_calories)
+        .try_fold(0, |max_calories, calories| {
+            calories.map(|calories| calories.max(max_calories))
+        })?;
+
+    println!("Part 1 answer = {max_calories}");
+
+    Ok(())
+}
+
+fn part2(input: &str) -> AnyResult {
     let elves = input.split("\n\n");
 
     let maxima = elves
-        .map(|elf| elf.lines().flat_map(str::parse::<u32>).sum::<u32>())
-        .fold([0, 0, 0], |mut maxima, calories| {
-            if calories > maxima[2] {
-                maxima[0] = maxima[1];
-                maxima[1] = maxima[2];
-                maxima[2] = calories;
-            } else if calories > maxima[1] {
-                maxima[0] = maxima[1];
-                maxima[1] = calories;
-            } else if calories > maxima[0] {
-                maxima[0] = calories;
-            }
+        .map(elf_to_calories)
+        .try_fold([0, 0, 0], |mut maxima, calories| {
+            calories.map(|calories| {
+                if calories > maxima[2] {
+                    maxima[0] = maxima[1];
+                    maxima[1] = maxima[2];
+                    maxima[2] = calories;
+                } else if calories > maxima[1] {
+                    maxima[0] = maxima[1];
+                    maxima[1] = calories;
+                } else if calories > maxima[0] {
+                    maxima[0] = calories;
+                }
 
-            maxima
-        });
+                maxima
+            })
+        })?;
 
     let top_three_calorie_sum: u32 = maxima.into_iter().sum();
 
     println!("Part 2 answer = {top_three_calorie_sum}");
+
+    Ok(())
 }
