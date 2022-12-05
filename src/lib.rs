9f72f16f5826a386add3329ab6c4f6cfde517537
jack-cooper <jack.cooper@stratumfive.com>
2022-12-05 16:50:32 +0000

    feat(all): Replaced generic lifetime in `CustomError` with concrete 'static

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,13 @@ use std::{borrow::Cow, error::Error, fmt};
 pub type AnyResult = Result<(), Box<dyn Error>>;
 
 #[derive(Debug)]
-pub struct CustomError<'a> {
-    pub msg: Cow<'a, str>,
+pub struct CustomError {
+    pub msg: Cow<'static, str>,
 }
 
-impl Error for CustomError<'_> {}
+impl Error for CustomError {}
 
-impl fmt::Display for CustomError<'_> {
+impl fmt::Display for CustomError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "CustomError({})", self.msg)
     }
