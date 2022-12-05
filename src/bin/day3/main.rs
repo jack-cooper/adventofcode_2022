3d0ba2d20f82efefc9bb59db0860e508240d216b
jack-cooper <jack.cooper@stratumfive.com>
2022-12-05 16:51:08 +0000

    feat(day3): Improved error handling

--- src/bin/day3/main.rs
+++ src/bin/day3/main.rs
@@ -1,17 +1,20 @@
-use std::{collections::HashSet, error::Error, fs};
+use std::{collections::HashSet, fs};
 
-fn main() -> Result<(), Box<dyn Error>> {
+use adventofcode_2022::{AnyResult, CustomError};
+
+fn main() -> AnyResult {
     let input = fs::read_to_string("src/bin/day3/input.txt")?;
 
-    part1(&input);
-    part2(&input);
+    part1(&input)?;
+    part2(&input)?;
 
     Ok(())
 }
 
-fn part1(input: &str) {
-    let priority_sum: u32 = input
+fn part1(input: &str) -> AnyResult {
+    let priority_sum = input
         .lines()
+        // Split the rucksack into 2 compartments, and convert the first into a set
         .map(|rucksack| {
             let (first, second) = rucksack.split_at(rucksack.len() / 2);
 
@@ -19,18 +22,22 @@ fn part1(input: &str) {
 
             (second, first_compartment_item_types)
         })
-        .flat_map(find_duplicate_item_type)
-        .map(get_priority)
-        .sum();
+        .map(find_duplicate_item_type)
+        .map(|duplicate| duplicate.map(get_priority))
+        .sum::<Result<u32, _>>()?;
 
     println!("Part 1 answer = {priority_sum}");
+
+    Ok(())
 }
 
-fn part2(input: &str) {
+fn part2(input: &str) -> AnyResult {
     let mut rucksacks: Vec<_> = input.lines().collect();
 
     let priority_sum: u32 = rucksacks
         .chunks_exact_mut(3)
+        // Converts the shortest 2 rucksacks into sets, takes their intersection,
+        // then iterates through the longest until it finds a duplicate
         .map(|squad_rucksacks| {
             squad_rucksacks.sort_unstable_by_key(|rucksack| rucksack.len());
 
@@ -44,21 +51,29 @@ fn part2(input: &str) {
 
             (squad_rucksacks[2], shared_item_types)
         })
-        .flat_map(find_duplicate_item_type)
-        .map(get_priority)
-        .sum();
+        .map(find_duplicate_item_type)
+        .map(|duplicate| duplicate.map(get_priority))
+        .sum::<Result<u32, _>>()?;
 
     println!("Part 2 answer = {priority_sum}");
+
+    Ok(())
 }
 
+/// Returns the first item from `container` which is in `already_seen_items`,
+/// or an error if none exists.
 fn find_duplicate_item_type(
     (container, already_seen_items): (&str, HashSet<char>),
-) -> Option<char> {
+) -> Result<char, CustomError> {
     container
         .chars()
         .find(|char| already_seen_items.contains(char))
+        .ok_or(CustomError {
+            msg: "No duplicate item was detected.".into(),
+        })
 }
 
+/// Returns the priority value of the given item
 fn get_priority(item: char) -> u32 {
     if item.is_uppercase() {
         (item as u32) - ('A' as u32) + 27
