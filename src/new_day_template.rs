37e8bf0f5a7a23b32f91694340451833cafc15fb
jack-cooper <cooper.jackwilliam@gmail.com>
2022-12-07 18:27:30 +0000

    feat(all): Added a template file for starting a new day

---
+++ src/new_day_template.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+use adventofcode_2022::AnyResult;
+
+fn main() -> AnyResult {
+    let input = fs::read_to_string("src/bin/dayX/input.txt")?;
+
+    part1(&input)?;
+    part2(&input)?;
+
+    Ok(())
+}
+
+fn part1(input: &str) -> AnyResult {
+    Ok(())
+}
+
+fn part2(input: &str) -> AnyResult {
+    Ok(())
+}
